ab3ee61381824741c96491b25eb51c80cb09ba68
ellrussbest <ellrussbest@gmail.com>
2023-08-01 17:59:09 +0200

    sucker for pain

--- c_third_lesson/src/service_client.cpp
+++ c_third_lesson/src/service_client.cpp
@@ -12,15 +12,15 @@ int main(int argc, char **argv) {
 
     c_third_lesson::Service service;
 
-    // service.request.a = a;
-    // service.request.b = b;
+    service.request.a = a;
+    service.request.b = b;
 
-    // if(client.call(service)) {
-    //     ROS_INFO("Sum %d", (long int)service.response.sum);
-    // }else {
-    //     ROS_ERROR("Failed to call service add_two_ints");
-    //     return 1;
-    // }
+    if(client.call(service)) {
+        ROS_INFO("Sum %ld", (long int)service.response.sum);
+    }else {
+        ROS_ERROR("Failed to call service add_two_ints");
+        return 1;
+    }
 
     return 0;
 }
--- c_third_lesson/src/service_node.cpp
+++ c_third_lesson/src/service_node.cpp
@@ -6,20 +6,20 @@ bool add(
     c_third_lesson::Service::Response &response
     )
 {
-    // response.sum = request.a + request.b;
-    // ROS_INFO("request: x=%ld, y=%ld", (long int)request.a, (long int)request.b);
-    // ROS_INFO("sending back response: [%ld]", (long int)response.sum);
+    response.sum = request.a + request.b;
+    ROS_INFO("request: x=%ld, y=%ld", (long int)request.a, (long int)request.b);
+    ROS_INFO("sending back response: [%ld]", (long int)response.sum);
     return true;
 }
 
 int main(int argc, char **argv) {
-    // ros::init(argc, argv, "service_node");
+    ros::init(argc, argv, "service_node");
 
-    // ros::NodeHandle nh;
+    ros::NodeHandle nh;
 
-    // ros::ServiceServer service = nh.advertiseService("add_two_ints", add);
-    // ROS_INFO("Ready to add two ints.");
-    // ros::spin();
+    ros::ServiceServer service = nh.advertiseService("add_two_ints", add);
+    ROS_INFO("Ready to add two ints.");
+    ros::spin();
 
     return 0;
 }
