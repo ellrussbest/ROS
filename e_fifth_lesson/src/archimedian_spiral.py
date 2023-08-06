a236dcf0164a0ce2967acd8d5cb865a98d85a4fb
ellrussbest <ellrussbest@gmail.com>
2023-08-06 22:59:13 +0200

    archimedian_spiral

--- e_fifth_lesson/src/archimedian_spiral.py
+++ e_fifth_lesson/src/archimedian_spiral.py
@@ -9,49 +9,70 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import math
 
 def move_spiral():
+    
+    # declare variables
+    flag: bool = False
+    x0: float
+    y0: float
+    x: float
+    y: float
+    theta: float
+    sampling_time: float = 0.1
+    linear_velocity: float = 2.0
+
+    # position subscriber callback
+    def callback(message: Pose):
+        nonlocal x, y, theta, flag, x0, y0
+
+        if(not flag):
+            x0 = message.x
+            y0 = message.y
+            flag = True
+        x = message.x
+        y = message.y
+        theta = message.theta
+
     rospy.init_node('spiral_movement', anonymous=True)
+    rospy.Subscriber("/turtle1/pose", Pose, callback=callback)
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
     vel_msg = Twist()
+    rospy.wait_for_message("/turtle1/pose", Pose)
 
-    # Set the desired linear velocity
-    vel_msg.linear.x = 0.5  # Adjust the value to control the speed
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
-
-    # Set the desired angular velocity (for rotation)
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0.2  # Adjust the value to set the angular speed
-
-    radius = 0.1  # Adjust the value to set the radius of the spiral
-    distance = 0
-    angle = 0
-
-    rate = rospy.Rate(10)  # Loop rate in Hz
+    # rate = rospy.Rate(10)  # Loop rate in Hz
+    i = 1
     while not rospy.is_shutdown():
-        # Calculate the current distance and angle traveled
-        distance += vel_msg.linear.x / 10.0
-        angle += vel_msg.angular.z / 10.0
+        # radius = math.sqrt(((x0 - x) **2) + ((y0 - y) **2))
+        vel_msg.linear.x = linear_velocity
+        # vel_msg.angular.z = (radius - theta)
+        # # / sampling_time
+        # velocity_publisher.publish(vel_msg)
+        # # rate.sleep()
+        # Calculate polar coordinates of points
+        i += 1
+        r0 = math.sqrt((x0**2) + (y0**2))
+        r1 = math.sqrt((x**2) + (y**2))
+        theta0 = math.atan2(y0, x0)
+        theta1 = math.atan2(y, x)
 
-        # Use Archimedean spiral equation to set x and y position
-        vel_msg.linear.x = radius * angle * rospy.get_time()
-        vel_msg.linear.y = radius * angle * rospy.get_time()
+        # print("%s %s %s %s"%(r0, r1, theta0, theta1))
 
-        # Keep moving in the spiral until a desired distance is covered
-        if distance >= 5.0:  # Adjust the value to set the desired distance
-            break
+        # Calculate change in angle and change in radius
+        delta_theta = theta1 - theta0
 
-        # Publish the velocity message
+        delta_r = r1 - r0
+        if delta_r == 0:
+            continue
+            # delta_r = 0.001
+
+        # Calculate angular velocity
+        vel_msg.angular.z = (delta_theta / delta_r) * linear_velocity
+
         velocity_publisher.publish(vel_msg)
-        rate.sleep()
 
-    # Stop the robot when the desired distance is covered
-    vel_msg.linear.x = 0
-    vel_msg.linear.y = 0
-    vel_msg.angular.z = 0
-    velocity_publisher.publish(vel_msg)
 
 if __name__ == '__main__':
     try:
