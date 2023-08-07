848beda65ca17c163dc337d04c8fb7caf649da9c
ellrussbest <ellrussbest@gmail.com>
2023-08-07 13:55:01 +0200

    some changes

---
+++ e_fifth_lesson/src/square_spiral.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+'''
+    1. Archimedian spiral
+    2. Euler spiral
+    3. Logarithmic spiral
+    4. Fibonacci spiral
+    5. Square Spiral
+'''
+
+import rospy
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
+import math
+
+def move_spiral():
+    return None
+
+if __name__ == "__main__":
+    print("Hello world")
